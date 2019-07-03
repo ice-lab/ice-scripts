67659293c20505bfc7fd69261d206711a4de8f8f
ClarkXia <xiawenwu41@gmail.com>
2019-07-03 14:49:20 +0800

    fix: modify babel config when jest test

--- packages/ice-scripts/lib/config/jest/babelTransform.js
+++ packages/ice-scripts/lib/config/jest/babelTransform.js
@@ -1,6 +1,16 @@
 const babelJest = require('babel-jest');
-const babelConfig = require('../getBabelConfig');
+const getBabelConfig = require('../getBabelConfig');
 
-module.exports = babelJest.createTransformer({
-  ...babelConfig(),
+const babelConfig = getBabelConfig();
+// options for @babel/preset-env when test
+babelConfig.presets = babelConfig.presets.map((preset) => {
+  if (Array.isArray(preset) && preset[0].indexOf('@babel/preset-env') > -1) {
+    return [preset[0], {
+      targets: {
+        node: 'current',
+      },
+    }];
+  }
+  return preset;
 });
+module.exports = babelJest.createTransformer(babelConfig);
