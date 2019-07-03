973449399c35e07182cf25c3ff24936011d93c4b
ClarkXia <xiawenwu41@gmail.com>
2019-07-03 14:50:27 +0800

    test: add test case for test command

---
+++ packages/ice-scripts/test/jest/jest.test.js
@@ -0,0 +1,16 @@
+const { execSync } = require('child_process');
+
+describe('jest test', () => {
+  test('typescript test', () => {
+    execSync('cd test/jest/ts && node ../../../bin/ice-scripts-test.js');
+    expect(1).toBe(1);
+  });
+  test('alias test', () => {
+    execSync('cd test/jest/alias && node ../../../bin/ice-scripts-test.js');
+    expect(1).toBe(1);
+  });
+  test('custom config test', () => {
+    execSync('cd test/jest/customConfig && node ../../../bin/ice-scripts-test.js');
+    expect(1).toBe(1);
+  });
+});
---
+++ packages/ice-scripts/test/jest/ts/sum.spec.ts
@@ -0,0 +1,5 @@
+import { sum } from './utils';
+
+test('typescript', () => {
+  expect(sum(1, 2)).toBe(3);
+});
