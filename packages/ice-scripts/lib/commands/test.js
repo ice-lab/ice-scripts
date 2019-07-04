9554b20fdae9d79f16ffae14c7bae61dda59e9b2
ClarkXia <xiawenwu41@gmail.com>
2019-07-04 11:52:04 +0800

    feat: add test command for ice-scripts (#6)
    * feat: add command test
    
    * fix: add alias module
    
    * fix: regex for test files
    
    * fix: modify babel config when jest test
    
    * test: add test case for test command

---
+++ packages/ice-scripts/lib/commands/test.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const fs = require('fs');
+const jest = require('jest');
+
+module.exports = (context) => {
+  const { commandArgs: { jestArgv }, rootDir, webpackConfig } = context;
+
+  // get user jest config
+  const jestConfigPath = jestArgv.config
+    ? path.join(rootDir, jestArgv.config)
+    : path.join(rootDir, 'jest.config.js');
+  let userJestConfig = {};
+  if (fs.existsSync(jestConfigPath)) {
+    userJestConfig = require(jestConfigPath); // eslint-disable-line
+  }
+
+  // get webpack.resolve.alias
+  const { alias } = webpackConfig.resolve;
+  const aliasModuleNameMapper = {};
+  Object.keys(alias || {}).forEach((key) => {
+    const aliasPath = alias[key];
+    // check path if it is a directory
+    if (fs.existsSync(aliasPath) && fs.statSync(aliasPath).isDirectory()) {
+      aliasModuleNameMapper[`^${key}/(.*)$`] = `${aliasPath}/$1`;
+    }
+    aliasModuleNameMapper[`^${key}$`] = aliasPath;
+  });
+
+  // generate default jest config
+  const jestConfig = {
+    rootDir,
+    setupFiles: [require.resolve('@babel/polyfill')],
+    testMatch: ['**/?*.(spec|test).(j|t)s?(x)'],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': require.resolve('../config/jest/babelTransform.js'),
+      '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': require.resolve('../config/jest/fileTransform.js'),
+    },
+    transformIgnorePatterns: [
+      '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
+      '^.+\\.module\\.(css|sass|scss|less)$',
+    ],
+    moduleNameMapper: {
+      '\\.(css|less|sass|scss)$': require.resolve('identity-obj-proxy'),
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': require.resolve('../config/jest/fileMock.js'),
+      ...aliasModuleNameMapper,
+    },
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+    testPathIgnorePatterns: ['/node_modules/'],
+    ...userJestConfig,
+  };
+
+  return new Promise((resolve, reject) => {
+    jest.runCLI(
+      {
+        ...jestArgv,
+        config: JSON.stringify(jestConfig),
+      },
+      [rootDir],
+    ).then((res) => {
+      const { results } = res;
+      if (results.success) {
+        resolve();
+      } else {
+        reject(new Error('Jest failed'));
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+};
---
+++ packages/ice-scripts/test/jest/alias/ice.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
+};
