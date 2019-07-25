7508961bb0b71a72981896ead98fab2867d5410a
ClarkXia <xiawenwu41@gmail.com>
2019-07-25 16:22:07 +0800

    chore: lint

--- packages/ice-scripts/bin/ice-scripts.js
+++ packages/ice-scripts/bin/ice-scripts.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander');
-const packageInfo = require('../package.json');
 const updater = require('update-notifier');
+const packageInfo = require('../package.json');
 const checkNodeVersion = require('../lib/utils/checkNodeVersion');
 const validationSassAvailable = require('../lib/utils/validationSassAvailable');
 
