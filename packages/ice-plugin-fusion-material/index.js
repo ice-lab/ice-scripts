fba7cce0f1eaccc11a22e84ea3974f2606be1789
skysuka <793695775@qq.com>
2019-11-19 11:40:16 +0800

    feat: add ice-plugin-fusion-material (#69)
    * feat: add ice-plugin-fusion-material
    
    * feat: ice-plugin-fusion-materia ver_0.0.2
    
    * feat: making ice-plugin-fusion-material plugin more easier to use
    
    * feat: make fusion-material plugin more easier to use
    
    * feat: fix lint problem
    
    * feat: add function to delete iframe tag in html
    
    * docs: update readme
    
    * docs: update package.json

---
+++ packages/ice-plugin-fusion-material/index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const fs = require('fs');
+const screenShot = require('./utils/screenShot');
+
+// Update package.json
+const updatePackageJson = (packageJsonPath, type, htmls, screenshots) => {
+  const jsonData = fs.readFileSync(packageJsonPath, 'utf8');
+  const jsonObj = JSON.parse(jsonData);
+  for (let i = 0; i < htmls.length; i++) {
+    jsonObj[type].views[i].html = htmls[i];
+    jsonObj[type].views[i].screenshot = screenshots[i];
+  }
+  fs.writeFileSync(packageJsonPath, JSON.stringify(jsonObj, null, '  '));
+};
+
+module.exports = ({
+  context,
+  onHook,
+}) => {
+  // after the build script finished
+  onHook('afterBuild', () => {
+    // get package.json and rootDir
+    const { pkg, rootDir } = context;
+    const packageJsonPath = path.join(rootDir, 'package.json');
+    const outputRootPath = path.join(rootDir, 'build', 'views');
+    fs.mkdirSync(outputRootPath);
+    // get htmls and screenshots if blockConfig.views or scaffoldConfig.views exist
+    const htmls = [];
+    const screenshots = [];
+    if (pkg.blockConfig && pkg.blockConfig.views) {
+      // block type
+      screenShot(rootDir, '/build/index.html', './build/views/block_view1.png', './build/views/block_view1.html', 2000);
+      updatePackageJson(packageJsonPath, 'blockConfig', ['build/views/block_view1.html'], ['build/views/block_view1.png']);
+    } 
+    else if (pkg.scaffoldConfig && pkg.scaffoldConfig.views) {
+      // scaffold type
+      for (let i = 0; i < pkg.scaffoldConfig.views.length; i++) {
+        screenShot(rootDir, '/build/index.html', `./build/views/page${i}.png`, `./build/views/page${i}.html`, 2000, pkg.scaffoldConfig.views[i].path);
+        htmls.push(`build/views/page${i}.html`);
+        screenshots.push(`build/views/page${i}.png`);
+      }
+      updatePackageJson(packageJsonPath, 'scaffoldConfig', htmls, screenshots);
+    }
+  });
+};
