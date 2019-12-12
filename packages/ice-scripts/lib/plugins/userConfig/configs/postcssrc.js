2eade980b7582ca85fc900e6584028fb59b113a9
daysai <wddpwzz36@gmail.com>
2019-12-12 16:46:49 +0800

    feat: add postcssrc

---
+++ packages/ice-scripts/lib/plugins/userConfig/configs/postcssrc.js
@@ -0,0 +1,24 @@
+module.exports = ({ chainWebpack }, postcssrc) => {
+  if (postcssrc) {
+    chainWebpack(config => {
+      // remove postcss-loader plugins in options
+      [
+        "scss",
+        "scss-module",
+        "css",
+        "css-module",
+        "less",
+        "less-module",
+        "styl",
+        "styl-module",
+      ].forEach(rule => {
+        if (config.module.rules.get(rule)) {
+          config.module
+            .rule(rule)
+            .use("postcss-loader")
+            .tap(({ plugins, ...others }) => ({ ...others }));
+        }
+      });
+    });
+  }
+};
